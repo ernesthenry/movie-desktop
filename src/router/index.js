50cee282f83e4a70a413f9a481e35bc81e6ddd30
ernesthenry <henry38ernest@gmail.com>
2023-08-15 11:30:53 +0300

    Fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+// import { createRouter, createWebHistory } from "vue-router";
+// import HomeView from "../views/HomeView.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
+// const routes = [
+//   {
+//     path: "/",
+//     name: "home",
+//     component: HomeView,
+//   },
+//   {
+//     path: "/about",
+//     name: "about",
 
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+//   },
+// ];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes,
+// });
 
-export default router;
+// export default router;
